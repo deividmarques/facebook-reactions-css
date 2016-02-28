56d8ad484bbc117b833121daa5413c90a4996f53
Deivid Marques <deividmarques@gmail.com>
2016-02-28 00:57:22 -0300

    removing comments

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,11 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 
 gulp.task('sass', function() {
-  return gulp.src('app/sass/**/*.sass') // Gets all files ending with .sass in app/sass and children dirs
+  return gulp.src('app/sass/**/*.sass')
     .pipe(sass())
     .pipe(gulp.dest('app/css'))
 })
 
-
 gulp.task('watch', function(){
-  gulp.watch('app/sass/**/*.sass', ['sass']); 
-  // Other watchers
+  gulp.watch('app/sass/**/*.sass', ['sass']);
 })
